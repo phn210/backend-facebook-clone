92bae50d8ec3b8fcf9de90e1c5c1af2b0f668c91
ndtr2000 <53716352+ndtr2000@users.noreply.github.com>
2022-12-01 11:32:34 +0700

    add Like and Post

--- src/api/controllers/posts.controller.js
+++ src/api/controllers/posts.controller.js
@@ -1,3 +1,5 @@
+const { getPostById } = require("../services/posts.service")
+
 async function getListPosts(req, res, next) {
     try {
         res.send({'test': 'OK'})
@@ -8,7 +10,35 @@ async function getListPosts(req, res, next) {
 
 async function getPost(req, res, next) {
     try {
-        res.send({'test': 'OK'})
+        const id = req.body.id;
+        const post = await getPostById(id);
+        res.status(200).send({
+            code: "1000",
+            message: "OK",
+            data: {
+                id: post._id,
+                content: post.content ? post.content : null,
+                created: post.created.toString(),
+                modified: post.modified.toString(),
+                like: post.likedUser.length.toString(),
+                comment: post.comments.length.toString(),
+                is_liked: user ? (post.likedUser.includes(user._id) ? "1": "0") : "0",
+                image: post.image.length > 0 ? post.image.map(image => { return {id: image._id, url: image.url};}) : null,
+                video: post.video.url ? {
+                    url: post.video.url,
+                    thumb: null
+                } : null,
+                author: post.author ? {
+                    id: post.author._id,
+                    name: post.author.name ? post.author.name : null,
+                    avatar: post.author.avatar.url ? post.author.avatar.url: null
+                } : null,
+                state: post.status ? post.status : null,
+                is_blocked: is_blocked(user, post.author),
+                can_edit: can_edit(user, post.author),
+                can_comment: "1"
+            }
+        })
     } catch (error) {
         res.status(500).send({'error': 'ERROR'})
     }
@@ -16,6 +46,9 @@ async function getPost(req, res, next) {
 
 async function addPost(req, res, next) {
     try {
+        let user = req.user;
+        req.post.author = user.id;
+        
         res.send({'test': 'OK'})
     } catch (error) {
         res.status(500).send({'error': 'ERROR'})
