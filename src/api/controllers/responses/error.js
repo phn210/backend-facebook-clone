6ef552e8d00d1ee0e55348618cdc2b2287a2e2c9
Nguyen Pham <phamhonguyen210@gmail.com>
2023-02-06 23:04:50 +0800

    update http codes

--- src/api/controllers/responses/error.js
+++ src/api/controllers/responses/error.js
@@ -1,48 +1,48 @@
 module.exports = {
     POST_IS_NOT_EXISTED: {
-        statusCode: 404,
+        statusCode: 200,
         body: {
             code: "9992",
             message: "Post is not existed"
         }
     },
     CODE_VERIFY_IS_INCORRECT: {
-        statusCode: 401,
+        statusCode: 200,
         body: {
             code: "9993",
             message: "Code verify is incorrect"
         }
     },
     NO_DATA_OR_END_OF_LIST_DATA: {
-        statusCode: 404,
+        statusCode: 200,
         body: {
             code: "9994",
             message: "No data or end of list data"
         }
     },
     USER_IS_NOT_VALIDATED: {
-        statusCode: 401,
+        statusCode: 200,
         body: {
             code: "9995",
             message: "User is not validated"
         }
     },
     USER_EXISTED: {
-        statusCode: 400,
+        statusCode: 200,
         body: {
             code: "9996",
             message: "User existed"
         }
     },
     METHOD_IS_INVALID: {
-        statusCode: 405,
+        statusCode: 200,
         body: {
             code: "9997",
             message: "Method is invalid"
         }
     },
     TOKEN_IS_INVALID: {
-        statusCode: 401,
+        statusCode: 200,
         body: {
             code: "9998",
             message: "Token is invalid"
@@ -63,21 +63,21 @@ module.exports = {
         }
     },
     PARAMETER_IS_NOT_ENOUGH: {
-        statusCode: 400,
+        statusCode: 200,
         body: {
             code: "1002",
             message: "Parameter is not enough"
         }
     },
     PARAMETER_TYPE_IS_INVALID: {
-        statusCode: 400,
+        statusCode: 200,
         body: {
             code: "1003",
             message: "Parameter type is invalid"
         }
     },
     PARAMETER_VALUE_IS_INVALID: {
-        statusCode: 400,
+        statusCode: 200,
         body: {
             code: "1004",
             message: "Parameter value is invalid"
@@ -91,49 +91,49 @@ module.exports = {
         }
     },
     FILE_SIZE_IS_TOO_BIG: {
-        statusCode: 400,
+        statusCode: 200,
         body: {
             code: "1006",
             message: "File size is too big"
         }
     },
     UPLOAD_FILE_FAILED: {
-        statusCode: 500,
+        statusCode: 200,
         body: {
             code: "1007",
             message: "Upload file failed"
         }
     },
     MAXIMUM_NUMBER_OF_IMAGES: {
-        statusCode: 400,
+        statusCode: 200,
         body: {
             code: "1008",
             message: "Maximum number of images"
         }
     },
     NOT_ACCESS: {
-        statusCode: 403,
+        statusCode: 200,
         body: {
             code: "1009",
             message: "Not access"
         }
     },
     ACTION_HAS_BEEN_DONE_PREVIOUSLY_BY_THIS_USER: {
-        statusCode: 400,
+        statusCode: 200,
         body: {
             code: "1010",
             message: "Action has been done previously by this user"
         }
     },
     COULD_NOT_PUBLISH_THIS_POST: {
-        statusCode: 400,
+        statusCode: 200,
         body: {
             code: "1011",
             message: "Could not publish this post"
         } 
     },
     LIMITED_ACCESS: {
-        statusCode: 400,
+        statusCode: 200,
         body: {
             code: "1012",
             message: "Limited access"
--- src/lib/utils/index.js
+++ src/lib/utils/index.js
@@ -1,1 +1,27 @@
-module.exports = {}
+const removeAccent = (str) => {
+    const ACCENTS_MAP = [
+      "aàảãáạăằẳẵắặâầẩẫấậ",
+      "AÀẢÃÁẠĂẰẲẴẮẶÂẦẨẪẤẬ",
+      "dđ", "DĐ",
+      "eèẻẽéẹêềểễếệ",
+      "EÈẺẼÉẸÊỀỂỄẾỆ",
+      "iìỉĩíị",
+      "IÌỈĨÍỊ",
+      "oòỏõóọôồổỗốộơờởỡớợ",
+      "OÒỎÕÓỌÔỒỔỖỐỘƠỜỞỠỚỢ",
+      "uùủũúụưừửữứự",
+      "UÙỦŨÚỤƯỪỬỮỨỰ",
+      "yỳỷỹýỵ",
+      "YỲỶỸÝỴ"    
+    ];
+    for (var i=0; i<ACCENTS_MAP.length; i++) {
+      var re = new RegExp('[' + ACCENTS_MAP[i].substr(1) + ']', 'g');
+      var char = ACCENTS_MAP[i][0];
+      str = str.replace(re, char);
+    }
+    return str;
+  }
+
+module.exports = {
+    removeAccent
+}
